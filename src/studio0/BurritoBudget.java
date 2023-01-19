840a4dac4d6e25b4dd8f2921165351d8a0292c5e
Jared <Jared@LAPTOP-OI2CTABL.dhcp.wustl.edu>
2023-01-19 15:15:02 -0600

    hi

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,8 +8,8 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		double totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+  		double averageWeeklyBurritos = (totalBurritos / 4);
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
